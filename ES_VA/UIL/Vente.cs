3b6c6c6ecfd1f4ca8e11b3087a6be51d454bcca4
Samuel Gascon <2151866@cstj.qc.ca>
2023-05-02 16:06:08 -0400

    Debut

---
+++ ES_VA/UIL/Vente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotreMatricule
+{
+    public class Vente
+    {
+        private string nomProvince;
+        public string NomProvince { get; set; }
+
+
+        private string typeVehicule;
+        public string TypeVehicule { get; set; }
+
+
+        private int annee;
+        public int Annee { get; set; }
+
+
+        private double nbUnites;
+        public double NbUnites { get; set; }
+
+
+        private double mntPar1000;
+        public double MntPar1000 { get; set; }
+
+    }
+}
